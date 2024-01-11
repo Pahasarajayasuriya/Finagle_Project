3bc8ec959fb4e3b1576ef84a033d904d229e90dc
MWeerasinghe <mithunweerasinghe999@gmail.com>
2024-01-11 14:32:03 +0530

    setting up new branch uis

--- app/views/admin/admin_branches.view.php
+++ app/views/admin/admin_branches.view.php
@@ -1,139 +1,112 @@
+<?php
+$role = "Admin";
+
+$this->view('includes/header', $data);
+$this->view('includes/NavBar', $data);
+$this->view('includes/footer', $data);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <title>Admin Branch</title>
 
-    <!-- Montserrat Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<head>
+    <title>Branches</title>
+    <link rel="stylesheet" href="<?= ROOT?>/assets/css/admin_products.css">
 
-    <!-- Material Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
 
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="<?= ROOT?>/assets/css/admin_style.css">
-  </head>
-  <body>
-    <div class="grid-container">
-
-      <!-- Header -->
-      <header class="header">
-        <div class="menu-icon" onclick="openSidebar()">
-          <span class="material-icons-outlined">menu</span>
-        </div>
-        <div class="header-left">
-          <span class="material-icons-outlined">search</span>
-        </div>
-        <div class="header-right">
-          <span class="material-icons-outlined">notifications</span>
-          <span class="material-icons-outlined">email</span>
-          <span class="material-icons-outlined">account_circle</span>
-        </div>
-      </header>
-      <!-- End Header -->
-
-      <!-- Sidebar -->
-      <aside id="sidebar">
-        <div class="sidebar-title">
-          <div class="sidebar-brand">
-            <!-- <span class="material-icons-outlined">shopping_cart</span> --> ADMIN
-          </div>
-          <span class="material-icons-outlined" onclick="closeSidebar()">close</span>
-        </div>
-
-        <ul class="sidebar-list">
-          <li class="sidebar-list-item">
-            <a href="<?= ROOT?>/admin_dashboard" target="_self">
-              <span class="material-icons-outlined">dashboard</span> Dashboard
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_self">
-              <span class="material-icons-outlined">inventory_2</span> Products
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="<?= ROOT?>/admin_branch" target="_self">
-              <span class="material-icons-outlined">storefront</span> Branches
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">groups</span> Customers
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">local_shipping</span> Deliverers
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">manage_accounts</span> Employees
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">manage_accounts</span> Managers
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">featured_video</span> Advertisements
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">settings</span> Settings
-            </a>
-          </li>
-        </ul>
-      </aside>
-      <!-- End Sidebar -->
-
-      <!-- Main -->
-      <main class="main-container">
-        <div class="main-title">
-          <h2>BRANCH</h2>
-          <button id ='add_button'>Add Branch</button>
-        </div>
-            <!-- ======Begine of the table=====-->
-          <!-- <section class="attendance"> -->
-            <div class="attendance-list">
-              <!-- <h1>Attendance List</h1> -->
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th>Branch ID</th>
-                    <th>Branch Name</th>
-                    <th>Address</th>
-                    <th>Actions</th>              
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php foreach($rows as $row): ?>
-                  <tr>
-                    <td><?= esc($row->id) ?></td>
-                    <td><?= esc($row->name) ?></td>
-                    <td><?= esc($row->address) ?></td>
-                    <td><button>Edit</button> <button>Delete</button></td>                    
-                  </tr>
-                  <?php endforeach; ?>
-                </tbody>
-              </table>
+<body>
+    <div class="overlay" id="overlay"></div>
+    <div class="home-section">
+        <!-- content  -->
+        <section id="main" class="main">
+            <div class="ad_head">
+                <p class="ad_head_1">BRANCH<span> DETAILS</span></p>
             </div>
-          </section>
 
-          <!-- =====End of the table====-->
-      </main>
-      <div>
-        
-      </div>
-      <!-- End Main -->
+            <div class="form-header">
+                <form action="#">
+                    <div class="ad-form-input">
+                        <input type="search" id="search" placeholder="Search...">
+                        <button type="submit" class="ad-search-btn">
+                            <i class='bx bx-search'></i>
+                        </button>
+                    </div>
+                </form>
 
+                <input class="add-btn" type="button" onclick="openReport()" value="Add Branches">
+            </div>
+
+            <!-- Popup Container -->
+            <div class="popup-container" id="popupContainer">
+                <h2>Add a Branch</h2>
+                <div class="popup-content">
+
+
+
+                    <label for="name">Branch Name:</label>
+                    <input type="text" id="description" name="description" placeholder="Enter branch name">
+
+                    <label for="address">Address:</label>
+                    <input type="text" id="price" name="address">
+
+                    <label for="contact-number">Contact number:</label>
+                    <input type="text" id="Image" name="contact-number">
+
+                    <label for="open">Openning hours:</label>
+                    <input type="time" id="open" name="open">
+
+                    <label for="close">Closing hours:</label>
+                    <input type="time" id="close" name="close">
+
+
+                    <div class="buttons-container">
+                        <button class="cancel-btn" onclick="closePopup()">Cancel</button>
+                        <button class="submit-btn" onclick="submitForm()">Submit</button>
+                    </div>
+                </div>
+            </div>
+            <div class="advertisement-table">
+            <div class="advertisement-header">
+                <div class="ad-image"></div>
+                <div class="ad-id"> Branch ID</div>
+                <div class="ad-description">Branch Name</div>
+                <div class="ad-date">Address</div>
+            </div>
+            </div>
+
+            <div class="popup-container" id="editPopupContainer">
+                <h2>Edit the branch</h2>
+                <div class="popup-content">
+                    <label for="editDescription">Branch Name:</label>
+                    <input type="text" id="editDescription" name="editDescription" placeholder="Enter the branch name">
+
+                    <label for="editEndDate">Address:</label>
+                    <input type="text" id="editEndDate" name="editEndDate">
+
+                    <div class="buttons-container">
+                        <button class="cancel-btn" onclick="closeEditPopup()">Cancel</button>
+                        <button class="submit-btn" onclick="submitEditForm()">Submit</button>
+                    </div>
+                </div>
+            </div class="branch-container">
+            <div class="advertisement-record">
+                <div class="advertisement-image"> <img src="https://lh3.googleusercontent.com/p/AF1QipNFVt_67WFrJbjsHEQfxY691SYz3wxrn1Ioq5KC=s1360-w1360-h1020" alt="branch.id" class="customer-image"></div>
+                <div class="branch-id">13</div>
+                <div class="branch-name">Borella</div>
+                <div class="branch-loc">104/2, Main road, Borella</div>
+                <div class="advertisement-actions">
+                  <button class="edit-button" onclick="openEditPopupDialog('${branch.id}', '${branch.name}', '${branch.location}')">Edit Product</button>
+                </div>
+            
+             </div>
+            </div>
+
+        </section>
+        <!-- <script src="branch-admin.js"></script> -->
     </div>
-    <!-- Custom JS -->
-    <script src="<?= ROOT?>/assets/js/admin.js"></script>
-  </body>
+</body>
+
 </html>
