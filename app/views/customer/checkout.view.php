bfd6e4a3570afe5f0237b79268c1d073c80f0e74
Pahasara Jayasuriya <136761380+Pahasarajayasuriya@users.noreply.github.com>
2024-01-25 11:49:57 +0530

    disable buttons

--- app/views/customer/checkout.view.php
+++ app/views/customer/checkout.view.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/checkout.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/boxicons/2.1.0/css/boxicons.min.css">
-    
+
 
     <title>Payment</title>
 
@@ -21,29 +20,29 @@
             <div class="check_row">
                 <div class="check_col">
                     <div class="recipe_head">
-                        
+
                         <i class='bx bxs-calendar-check' style="margin-top: 10px;"></i>
                         <p class="recipe_head_1">ORDER<span> CHECKOUT</span></p>
                     </div>
-              
+
 
                     <div class="check_inline">
-                        <label class="check_name" for="check_name">Full Name</label>
-                        <input class="check_input" type="text" id="check_name" name="check_name">
+                        <label class="check_name" for="check_name">Name</label>
+                        <input class="check_input" value="<?= set_value('name') ?>" type="text" id="check_name" name="check_name">
                     </div>
                     <div class="check_inline">
                         <label class="check_name" for="check_comemail">E-mail</label>
-                        <input class="check_input" type="email" id="check_comemail" name="check_comemail">
+                        <input class="check_input" value="<?= set_value('email') ?>" type="email" id="check_comemail" name="check_comemail">
                     </div>
                     <div class="check_inline">
                         <label class="check_name" for="check_phoneno">Phone Number</label>
-                        <input class="check_input" type="text" id="check_phoneno" name="check_phoneno">
+                        <input class="check_input" value="<?= set_value('teleno') ?>" type="text" id="check_phoneno" name="check_phoneno">
                     </div>
 
                     <div class="check_inline">
                         <label class="check_name" for="check_option">Delivery or Pickup:</label>
                         <label class="check_radio">
-                            <input  type="radio" name="delivery-pickup" value="delivery"> Delivery
+                            <input type="radio" name="delivery-pickup" value="delivery"> Delivery
                         </label>
                         <label class="check_radio">
                             <input type="radio" name="delivery-pickup" value="pickup"> Pickup
@@ -53,70 +52,68 @@
 
                     <div class="check_inline">
                         <label class="check_name" for="delivery_orders"><b>For Delivery Orders:</b></label>
-                       
+
                         <a class="change_address" href="#">Choose Address</a>
                     </div>
 
-                    <div class="check_inline" >
-                    <label class="check_name" for="pickup_orders"><b>For Pickup Orders:</b></label>
-                    <select >
+                    <div class="check_inline">
+                        <label class="check_name" for="pickup_orders"><b>For Pickup Orders:</b></label>
+                        <select id="pickupLocation" name="pickupLocation" disabled>
                             <option class="branch_select" value="branch1">Pelawatta</option>
                             <option class="branch_select" value="branch2">Ja-Ela</option>
                             <option class="branch_select" value="branch3">Nugegoda</option>
-                            <option class="branch_select"value="branch3">Jawatta</option>
+                            <option class="branch_select" value="branch3">Jawatta</option>
                             <option class="branch_select" value="branch3">Kollupitiya</option>
-                            <option class="branch_select"value="branch3">Dehiwala</option>
-                            <option class="branch_select"value="branch3">Borella</option>
-                    </select>
+                            <option class="branch_select" value="branch3">Dehiwala</option>
+                            <option class="branch_select" value="branch3">Borella</option>
+                        </select>
                     </div>
 
-               
 
-                    <div class="check_flex"> 
+
+                    <div class="check_flex">
                         <div class="check_inputBox">
                             <label class="check_name" for="order_data">Date:</label>
-                    
-                            <input  class="check_input" type="date">
+
+                            <input class="check_input" type="date">
                         </div>
                         <div class="check_inputBox">
                             <label class="check_name" for="order_timr">Time:</label>
                             <input class="check_input" type="time">
                         </div>
-                    </div> 
-                  
+                    </div>
+
                     <div class="check_inline">
                         <label class="check_name" for="check_gift">Send as a gift:</label>
                         <label class="check_radio">
-                            <input  type="radio" name="gift" value="yes"> Yes
+                            <input type="radio" name="gift" value="yes" class="send-as-gift-radio"> Yes
                         </label>
                         <label class="check_radio">
-                            <input type="radio" name="not gift" value="no"> No
+                            <input type="radio" name="gift" value="no" class="send-as-gift-radio"> No
                         </label>
-
                     </div>
-                    
+
                     <div class="check_inline">
                         <label class="check_name" for="order_note">Note:</label><br><br>
-    
+
                         <textarea class="check_input" id="note" name="note"></textarea>
                     </div>
 
                 </div>
                 <div class="check_inline">
-                        <label class="check_name" for="check_payment">Payment Method :</label>
-                        <label class="check_radio_1">
-                            <input  type="radio" name="card" value="card"> Credit or Debit card
-                        </label>
-                        <label class="check_radio_1">
-                            <input type="radio" name="cash" value="cash"> Cash payment
-                        </label>
+                    <label class="check_name" for="check_payment">Payment Method :</label>
+                    <label class="check_radio_1">
+                        <input type="radio" name="payment" value="card" class="payment-method-radio"> Credit or Debit card
+                    </label>
+                    <label class="check_radio_1">
+                        <input type="radio" name="payment" value="cash" class="payment-method-radio"> Cash payment
+                    </label>
+                </div>
 
-                    </div>
 
-                    
 
                 <div class="check_col">
-                <br>
+                    <br>
                     <h3 class="check_title">payment</h3>
 
                     <div class="check_inline">
@@ -133,12 +130,12 @@
                         <label class="check_name" for="check_no">Credit Card number :</label>
                         <input class="check_input" type="text" id="check_no" name="check_name" placeholder="1111-2222-3333-4444">
                     </div>
-                  
-                    <div class="check_flex"> 
+
+                    <div class="check_flex">
                         <div class="check_inputBox">
                             <label class="check_name" for="check_month">Exp Month:</label>
-                    
-                            <input  class="check_input" type="text" placeholder="January">
+
+                            <input class="check_input" type="text" placeholder="January">
                         </div>
                         <div class="check_inputBox">
                             <label class="check_name" for="check_year">Exp Year:</label>
@@ -148,10 +145,10 @@
                             <label class="check_name" for="check_CVV">CVV :</label>
                             <input class="check_input" type="text" placeholder="123">
                         </div>
-                    </div> 
-                    
+                    </div>
 
-                    
+
+
 
 
                 </div>
@@ -176,7 +173,7 @@
             });
         </script>
     </div>
-
+    <script src="<?= ROOT ?>/assets/js/checkout.js"></script>
 </body>
 
 </html>
---
+++ public/assets/js/checkout.js
@@ -0,0 +1,44 @@
+const deliveryRadio = document.querySelector(
+  'input[name="delivery-pickup"][value="delivery"]'
+);
+const pickupRadio = document.querySelector(
+  'input[name="delivery-pickup"][value="pickup"]'
+);
+
+const sendAsGiftRadios = document.querySelectorAll('.send-as-gift-radio');
+const paymentMethodRadios = document.querySelectorAll('.payment-method-radio');
+
+const pickupLocation = document.getElementById("pickupLocation");
+
+deliveryRadio.addEventListener("change", enableDeliveryOptions);
+pickupRadio.addEventListener("change", enablePickupOptions);
+
+function enableDeliveryOptions() {
+  pickupLocation.disabled = true;
+}
+
+function enablePickupOptions() {
+  pickupLocation.disabled = false;
+}
+
+sendAsGiftRadios.forEach(function(radio) {
+    radio.addEventListener('click', function() {
+        sendAsGiftRadios.forEach(function(otherRadio) {
+            if (otherRadio !== radio) {
+                otherRadio.checked = false;
+            }
+        });
+    });
+});
+
+paymentMethodRadios.forEach(function(radio) {
+    radio.addEventListener('click', function() {
+        // Toggle the selection when the user clicks on a radio button
+        paymentMethodRadios.forEach(function(otherRadio) {
+            if (otherRadio !== radio) {
+                otherRadio.checked = false;
+            }
+        });
+    });
+});
+
