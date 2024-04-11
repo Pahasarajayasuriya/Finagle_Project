91b6f84e58e629e90561521d0af415cd6242a6ab
MWeerasinghe <mithunweerasinghe999@gmail.com>
2024-04-11 12:04:31 +0530

    moving on to mamp with bug fixing

--- app/core/config.php
+++ app/core/config.php
@@ -8,7 +8,7 @@ if($_SERVER['SERVER_NAME'] == 'localhost'){
     define('DBHOST', 'localhost');
     define('DBNAME', 'finagle');
     define('DBUSER', 'root');
-    define('DBPASS', '');
+    define('DBPASS', 'root');
     define('DBDRIVER', 'mysql');
 
     define('ROOT','http://localhost/finagle/public');
